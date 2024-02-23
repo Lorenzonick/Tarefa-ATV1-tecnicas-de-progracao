5fdc6e10e15246d9ca1306ab46ed08ca44da90f3
Nicolli Lorenzo <148282702+Lorenzonick@users.noreply.github.com>
2024-02-23 13:55:54 -0300

    tarefa2.java

--- tarefa2.java
+++ tarefa2.java
@@ -1,31 +1,18 @@
-importar  java . útil . Scanner ;
+import java.util.Scanner;
 
- classe  pública Principal {
-    public  static  void  main ( String [] args ) {
-        Scanner  scanner = novo  Scanner ( System . in );
-
-        Sistema . fora . println ( "Escolha uma opção:" );
-        Sistema . fora . println ( "1. Calcular área do quadrado" );
-        Sistema . fora . println ( "2. Calcular área do círculo" );
-
-        int  opcao = scanner . próximoInt ();
-
-        switch ( opção ) {
-            caso  1 :
-                Sistema . fora . println ( "Digite o lado do quadrado:" );
-                 lado duplo = scanner . próximoDuplo ();
-                 área duplaQuadrado = lado * lado ;
-                Sistema . fora . println ( "A área do quadrado e: " + areaQuadrado );
-                quebrar ;
-            caso  2 :
-                Sistema . fora . println ( "Digite o raio do círculo:" );
-                 raio duplo = scanner . próximoDuplo ();
-                 área duplaCirculo = Math . PI * raio * raio ;
-                Sistema . fora . println ( "A área do círculo e: " + areaCirculo );
-                quebrar ;
-            padrão :
-                Sistema . fora . println ( "Opção inválida." );
-                quebrar ;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o primeiro número:");
+        int numero1 = scanner.nextInt();
+        System.out.println("Digite o segundo número:");
+        int numero2 = scanner.nextInt();
+        if(numero1 == numero2){
+            System.out.println("Os números são iguais.");
+        } else if (numero1>numero2){
+           System.out.println("Os números são diferentes, e o primeiro número é maior");
+        } else{
+            System.out.println("Os números são diferentes, e segundo número é maior");
         }
     }
 }
