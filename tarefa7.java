037df763ac39a8d54c842fa673abd1dd075f3e59
Nicolli Lorenzo <148282702+Lorenzonick@users.noreply.github.com>
2024-02-23 13:58:22 -0300

    tarefa7.java

--- tarefa7.java
+++ tarefa7.java
@@ -1,16 +1,16 @@
-importar  java . útil . Scanner ;
+import java.util.Scanner;
 
- classe  pública Principal {
-    public  static  void  main ( String [] args ) {
-        Scanner  scanner = novo  Scanner ( System . in );
-    Sistema . fora . println ( "Digite sua idade: " );
-    int  idade = scanner . próximoInt ();
-    if ( idade < 18 ){
-        Sistema . fora . println ( "Você é menor de idade" );
-    } else  if ( idade < 60 ){
-        Sistema . fora . println ( "Você é adulto" );
-    } outro {
-        Sistema . fora . println ( "Você é idoso" );
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    System.out.println("Digite sua idade: ");
+    int idade = scanner.nextInt();
+    if(idade < 18){
+        System.out.println("Você é menor de idade");
+    } else if (idade < 60){
+        System.out.println("Você é adulto");
+    } else {
+        System.out.println("Você é idoso");
     }
     }
 }
