ab56184d05b35b6f5fcb14cf168e7bc48d20c227
Nicolli Lorenzo <148282702+Lorenzonick@users.noreply.github.com>
2024-02-23 13:59:08 -0300

    tarefa9.java

--- tarefa9.java
+++ tarefa9.java
@@ -1,23 +1,23 @@
-importar  java . útil . Scanner ;
-importar  java . útil . ListaArray ;
+import java.util.Scanner;
+import java.util.ArrayList;
 
- classe  pública Principal {
-    public  static  void  main ( String [] args ) {
-        Scanner  scanner = novo  Scanner ( System . in );
-        ArrayList < String > nomes = new  ArrayList < String >();
-
-        para ( int  eu = 0 ; eu < 5 ; eu ++) {
-            Sistema . fora . println ( "Digite o nome " + ( i + 1 ) + ":" );
-            nomes . adicionar ( scanner.nextLine ( )) ;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> nomes = new ArrayList<String>();
+       
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Digite o nome " + (i+1) + ":" );
+            nomes.add(scanner.nextLine());
         }
 
-        Sistema . fora . println ( "Digite outro nome: " );
-        String  outroNome = scanner . próximaLinha ();
-
-        if ( nomes . contém ( outroNome )) {
-            Sistema . fora . println ( "O nome " + outroNome + "está na lista!" );
-        } outro {
-            Sistema . fora . println ( "O nome " + outroNome + " não está na lista." );
+        System.out.println("Digite outro nome: ");
+        String outroNome = scanner.nextLine();
+        
+        if (nomes.contains(outroNome)) {
+            System.out.println("O nome " + outroNome + " está na lista!");
+        } else{ 
+            System.out.println("O nome " + outroNome + " não está na lista.");
         }
     }
 }
